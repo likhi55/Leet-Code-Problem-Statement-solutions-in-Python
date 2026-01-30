7a9e8dbca9b3f053001bdf29b22931e0884e0a98
likhi <likhithhn31@gmail.com>
2026-01-30 14:29:34 +0530

    2026-Jan-30

---
+++ 30_01_2026/AddBinary/main.py
@@ -0,0 +1,6 @@
+from solution import Solution
+
+my_solution = Solution()
+a = "1010"
+b = "1011"
+print(my_solution.add_binary(a, b))
---
+++ 30_01_2026/AddBinary/solution.py
@@ -0,0 +1,4 @@
+class Solution:
+    @staticmethod
+    def add_binary(a, b):
+        return bin(int(a, 2) + int(b, 2))[2:]
